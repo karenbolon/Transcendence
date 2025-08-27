4654239380f8d764927b161822017523ce0131c2
Karen Bolon <kbolon@gmail.com>
2025-08-27 10:24:04 +0000

    updated config files to use vite environment and now have light/dark mode working and game tested in development on Firefox and Chromium

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vitest/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  plugins: [
+    tailwindcss()
+  ],
   test: {
     environment: 'jsdom',        // DOM APIs for your canvas code
     include: ['src/**/*.test.ts'],

48c7d6f23aedaa472ed3ad41c8ee15ef5e4e2072
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2025-09-02 12:21:48 +0200

    updated merge conflict on postcss.config.ts for testing

--- frontend/postcss.config.ts
+++ frontend/postcss.config.ts
@@ -1,19 +1,8 @@
 // postcss.config.ts
-<<<<<<< HEAD
 
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
-=======
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
->>>>>>> origin/devops
 };

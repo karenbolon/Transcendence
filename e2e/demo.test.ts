efdc4562c6af3b54d896d95cfaa3736e27040a9a
kellenr <kellenramosph@gmail.com>
2026-01-26 22:14:24 +0100

    Migration of the base of the project, with Mock Version of a DATABASE

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -3,4 +3,5 @@ import { expect, test } from '@playwright/test';
 test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.locator('script')).toBeVisible();
+
 });
